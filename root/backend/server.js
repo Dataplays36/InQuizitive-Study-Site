baab8586fe651c5203aefb378e9aedb538209438
PappySunseed <calebruby12@gmail.com>
2025-01-28 19:59:41 -0500

    Update server.js

--- root/backend/server.js
+++ root/backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();  // Load environment variables from .env file
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // MySQL Connection
 const db = mysql.createConnection({
